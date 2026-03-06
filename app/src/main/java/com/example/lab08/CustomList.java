b8351fd49c007b50bfef4ba54e56fd0b2314192f
stanley-zhao <stanleyzhao.ca@outlook.com>
2026-03-05 18:11:48 -0700

    test4

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -2,6 +2,7 @@ package com.example.lab08;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class CustomList {
     private List<City> cities;
@@ -20,4 +21,22 @@ public class CustomList {
 
     // Will be implemented later using TDD (leave blank for now)
     // public boolean hasCity(City city) { ... }
+    /**
+     * This delete the city from the list if exists, otherwise throw an exception
+     */
+    public void delete(City city) {
+        if(hasCity(city)){
+            cities.remove(city);
+        }else{
+            throw new NoSuchElementException();
+        }
+    }
+
+    /**
+     * return number of cities in the list
+     * @return number of cities in the list
+     */
+    public int countCities(){
+        return cities.size();
+    }
 }
--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,6 +2,8 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.NoSuchElementException;
+
 public class CustomListTest {
     @Test
     public void testHasCity() {
@@ -10,5 +12,34 @@ public class CustomListTest {
         list.addCity(calgary);
 // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
+    }
+
+    @Test
+    void testDelete(){
+        CustomList cityList = new CustomList();
+        City city = new City("Charlottetown", "Prince Edward Island");
+        cityList.addCity(city);
+        cityList.delete(city);
+        assertFalse(cityList.hasCity(city));
+    }
+
+    @Test
+    void testDeleteException(){
+        CustomList cityList = new CustomList();
+        City city = new City("Charlottetown", "Prince Edward Island");
+        cityList.addCity(city);
+        assertThrows(NoSuchElementException.class, () -> {
+            cityList.delete(new City("Calgary", "Alberta"));
+        });
+    }
+
+    @Test
+    void testCountCities(){
+        CustomList cityList = new CustomList();
+        City city1 = new City("Charlottetown", "Prince Edward Island");
+        City city2 = new City("Halifax", "Nova Scotia");
+        cityList.addCity(city1);
+        cityList.addCity(city2);
+        assertEquals(2, cityList.countCities());
     }
 }
