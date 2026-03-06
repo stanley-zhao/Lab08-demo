108634965f9ff359b1e690f9c1045197ba7e57fe
stanley-zhao <stanleyzhao.ca@outlook.com>
2026-03-05 19:41:46 -0700

    finish test delete

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -18,20 +18,20 @@ public class CustomList {
         return cities.contains(city);
     }
 
-    // Will be implemented later using TDD (leave blank for now)
-    // public boolean hasCity(City city) { ... }
-//    /**
-//     * This delete the city from the list if exists, otherwise throw an exception
-//     */
-//    public void delete(City city) {
-//        if(hasCity(city)){
-//            //
-//            cities.remove(city);
-//        }else{
-//            throw new NoSuchElementException();
-//        }
-//    }
-//
+    /** Will be implemented later using TDD (leave blank for now)
+     public boolean hasCity(City city) { ... }
+    /**
+     * This delete the city from the list if exists, otherwise throw an exception
+     */
+    public void delete(City city) {
+        if(hasCity(city)){
+            //
+            cities.remove(city);
+        }else{
+            throw new NoSuchElementException();
+        }
+    }
+
 //    /**
 //     * return number of cities in the list
 //     * @return number of cities in the list
